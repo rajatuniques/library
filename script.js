17ed2db061aa476f500769f2fb9be0fc4afc4013
rajatuniques <176837105+rajatuniques@users.noreply.github.com>
2025-05-02 12:59:06 +0530

    Create array for books & function to add them

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
+const myLibrary = [];
+
 function Book (title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
-    this.getInto = function () {
-        let read_statement = "";
-        if(isRead===true) {
-            read_statement = "have been read!";
-        }
-        else {
-            read_statement = "not read yet!";
-        }
-        
-        final_statement = `${title} by ${author}, ${pages}, ${read_statement}`;
-        
-        console.log(final_statement);
-        return;
-    };
 };
+
+Book.prototype.getInfo = function () {
+    let read_statement = "";
+    if(this.isRead===true) {
+        read_statement = "have been read!";
+    }
+    else {
+        read_statement = "not read yet!";
+    }
+    
+    let final_statement = `${this.title} by ${this.author}, ${this.pages} pages, ${read_statement}`;
+    
+    console.log(final_statement);
+    return;
+};
+
+function addBookToLibrary(title, author, pages, isRead) {
+    let new_book = new Book(title, author, pages, isRead);
+    myLibrary.push(new_book);
+}
+
+addBookToLibrary("a", "b", 12, true);
+console.log(myLibrary[0].getInfo());
