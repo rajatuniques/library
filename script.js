6d81f3409b4006ee7b6b8a804f261462a4171fa3
rajatuniques <176837105+rajatuniques@users.noreply.github.com>
2025-05-03 22:38:28 +0530

    Enable remove book functionality

--- script.js
+++ script.js
@@ -19,8 +19,7 @@ Book.prototype.getInfo = function () {
     
     let final_statement = `${this.title} by ${this.author}, ${this.pages} pages, ${read_statement}`;
     
-    console.log(final_statement);
-    return;
+    return final_statement;
 };
 
 function addBookToLibrary(title, author, pages, isRead, id) {
@@ -28,10 +27,36 @@ function addBookToLibrary(title, author, pages, isRead, id) {
     myLibrary.push(new_book);
 }
 
+function removeBookFromLibrary(id) {
+    let target_book = null;
+    let target_book_idx = 0;
+    for(let i=0; i<myLibrary.length; i++) {
+        if(id === myLibrary[i].id) {
+            target_book = myLibrary[i];
+            target_book_idx = i;
+            break;
+        }
+    }
+    if(target_book === null) {
+        console.log("Book doesn't exist.");
+        return;
+    }
+    let info = myLibrary[target_book_idx].getInfo();
+    console.log(`Removed book : ${info}`);
+    myLibrary.splice(target_book_idx, 1);
+    return;
+}
+
 addBookToLibrary("a", "b", 12, true, crypto.randomUUID());
 addBookToLibrary("c", "d", 23, false, crypto.randomUUID());
 addBookToLibrary("e", "f", 74, true, crypto.randomUUID());
 
+for(let i=0; i<myLibrary.length; i++) {
+    console.log(myLibrary[i].getInfo());
+    console.log(myLibrary[i].id);
+}
+let sid = myLibrary[0].id;
+removeBookFromLibrary(sid);
 for(let i=0; i<myLibrary.length; i++) {
     console.log(myLibrary[i].getInfo());
     console.log(myLibrary[i].id);
