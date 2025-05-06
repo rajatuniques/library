16d00c860aab13578a41c9cafe2b38b6276d18cf
rajatuniques <176837105+rajatuniques@users.noreply.github.com>
2025-05-06 11:40:12 +0530

    Refine dialog book add feature

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const dialog_show = document.querySelector('header button');
 const dialog_close = document.querySelector('button.close');
 const dialog_add = document.querySelector('button[type="submit"]');
 const form = document.querySelector('dialog form');
+const reset_btn = document.querySelector('button[type="reset"]');
 
 parent_main.addEventListener("click", (e) => {
     const card = e.target.closest('.book');
@@ -36,42 +37,47 @@ parent_main.addEventListener("click", (e) => {
     }
 });
 
+function createBookCard(book) {
+    const book_card = document.createElement('div');
+    const book_title = document.createElement('h4');
+    const book_author = document.createElement('p');
+    const book_pages = document.createElement('p');
+
+    const read_label = document.createElement('label');
+    const read_btn = document.createElement('button');
+    const remove_btn = document.createElement('button');
+
+    book_card.classList.add('book');
+    book_card.id = book.id;
+
+    book_title.textContent = book.title;
+    book_author.textContent = `By ${book.author}`;
+    book_pages.textContent = `${book.pages} pages`;
+    
+    read_label.textContent = "Read?";
+    read_btn.textContent = book.isRead ? "Yes" : "No";
+    read_btn.classList.add("read");
+    remove_btn.textContent = "Remove";
+    remove_btn.classList.add("remove");
+
+    read_label.setAttribute('for', `read${book.id}`);
+    read_btn.id = `read${book.id}`;
+    
+    book_card.appendChild(book_title);
+    book_card.appendChild(book_author);
+    book_card.appendChild(book_pages);
+    book_card.appendChild(read_label);
+    book_card.appendChild(read_btn);
+    book_card.appendChild(remove_btn);
+
+    return book_card;
+}
+
 function displayBook () {
     const main = document.querySelector('main');
 
     for(let book of myLibrary) {
-        const book_card = document.createElement('div');
-        const book_title = document.createElement('h4');
-        const book_author = document.createElement('p');
-        const book_pages = document.createElement('p');
-
-        const read_label = document.createElement('label');
-        const read_btn = document.createElement('button');
-        const remove_btn = document.createElement('button');
-
-        book_card.classList.add('book');
-        book_card.id = book.id;
-
-        book_title.textContent = book.title;
-        book_author.textContent = `By ${book.author}`;
-        book_pages.textContent = `${book.pages} pages`;
-        
-        read_label.textContent = "Read?";
-        read_btn.textContent = book.isRead ? "Yes" : "No";
-        read_btn.classList.add("read");
-        remove_btn.textContent = "Remove";
-        remove_btn.classList.add("remove");
-
-        read_label.setAttribute('for', `read${book.id}`);
-        read_btn.id = `read${book.id}`;
-        
-        book_card.appendChild(book_title);
-        book_card.appendChild(book_author);
-        book_card.appendChild(book_pages);
-        book_card.appendChild(read_label);
-        book_card.appendChild(read_btn);
-        book_card.appendChild(remove_btn);
-
+        let book_card = createBookCard(book);
         main.appendChild(book_card);
     }
 }
@@ -87,19 +93,20 @@ dialog_close.addEventListener('click', () => {
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    const main = document.querySelector('main');
     const formData = new FormData(form);
     const new_title = formData.get('title');
     const new_author = formData.get('author');
     const new_pages = formData.get('pages');
     const new_isRead = 
         formData.get('isRead') === 'yes'? true : false;
+
+    console.log("Book data obtained");
     
-    for(let book of myLibrary) {
-        removeBookFromLibrary(book.id);
-    }
     addBookToLibrary(new_title, new_author, new_pages, new_isRead);
     dialog.close();
-    displayBook();
+    let book_card = createBookCard(myLibrary[myLibrary.length - 1]);
+    main.appendChild(book_card);
 });
 
 // code
